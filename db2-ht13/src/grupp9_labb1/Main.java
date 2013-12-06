814ef92e1d68f8376ccb72d439f0ac18364930dd
svalan <svalan@svalan-PC>
2013-12-06 13:51:54 +0100

    labb 1 uppstart

---
+++ db2-ht13/src/grupp9_labb1/Main.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupp9_labb1;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+
+        try {
+            DriverManager.registerDriver(new sun.jdbc.odbc.JdbcOdbcDriver());
+//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver"); // också ok
+        } catch (Exception e) {
+            System.out.println("Kan inte hitta database driver class: " + e);
+        }
+
+        Connection con = DriverManager.getConnection("JDBC:ODBC:grupp9;database=patientsystem;");
+        
+        DatabaseMetaData md = con.getMetaData();
+        
+        System.out.println(md.getDatabaseProductName());
+
+
+
+    }
+}
