f816bf01fb3859d12ffabb00bd6287f6a2958fad
Joel <Joel@Joel-Dator>
2013-12-09 18:25:06 +0100

    la till metoden dataAccessLayer.checkIfStudentExists(String pnr)

--- db2-ht13/src/grupp9_uppgift1/DataAccessLayer.java
+++ db2-ht13/src/grupp9_uppgift1/DataAccessLayer.java
@@ -7,6 +7,8 @@
 
 package grupp9_uppgift1;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Joel
@@ -31,6 +33,32 @@ public class DataAccessLayer {
         connection.commit();
         connection.close();
     }
+    
+    public boolean checkIfStudentExists(String pnr){
+        
+        Boolean resultBoolean;
+        try {
+            
+            String sqlString = "SELECT s.pnr FROM Student s WHERE (s.pnr = '" + pnr + "')";
+            Statement stmt = connection.createStatement();
+            ResultSet rset = stmt.executeQuery(sqlString);
+            
+            if(rset.next()){
+            resultBoolean = true;
+            System.out.println("studenten " + pnr + " finns redan");
+            }else{
+            resultBoolean = false;
+            System.out.println("Studenten " + pnr + " finns ej");
+            }
+        
+        return resultBoolean;
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessLayer.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return false;
+    }
+    
+            
 
     void updateStudent(String[] studentData) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
