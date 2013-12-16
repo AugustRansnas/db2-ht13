a8c85b15c064b5118ea4fdf3e60ae0cbccf5e6a5
Admin <Admin@Viktor>
2013-12-16 05:44:56 +0100

    character encoding funkar med svenska bokstäver

--- db2-ht13/src/grupp9_uppgift1/Main.java
+++ db2-ht13/src/grupp9_uppgift1/Main.java
@@ -9,14 +9,16 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 /**/
+
 /**
  *
  * @author Viktor Voigt
  * @author Jonas Ahrne
  * @author Joel Pennegård
  * @author August Ransnäs
- * 
+ *
  */
 
 public class Main {
@@ -24,29 +26,33 @@ public class Main {
     public static void main(String[] args) throws SQLException {
 
         try {
-            
+
             DriverManager.registerDriver(new sun.jdbc.odbc.JdbcOdbcDriver());
 
         } catch (SQLException e) {
-            
+
             System.out.println("Cant find driver class: " + e);
-            
+
         }
 
-        Connection connection = DriverManager.getConnection("JDBC:ODBC:grupp9;database=studentsystem;");
-        
+        Properties prop = new Properties();
+
+        prop.put("charSet", "iso-8859-1");
+
+        Connection connection = DriverManager.getConnection("JDBC:ODBC:grupp9;database=studentsystem;", prop);
+
         DatabaseMetaData dbmd = connection.getMetaData();
-        
+
         System.out.println("Connected to: " + dbmd.getDatabaseProductName());
-        
+
         DataAccessLayer dataAccessLayer = new DataAccessLayer(connection);
-        
+
         Controller controller = new Controller(dataAccessLayer);
 
         View mainView = new View(controller);
 
         mainView.setVisible(true);
- 
+
     }
 
 }
